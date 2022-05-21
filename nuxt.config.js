7bad0e9e9528739e80f8ecf546c8a7291a8c8130
realabdullah <abdulodesanmi@gmail.com>
2022-05-21 11:54:17 +0100

    Page Title

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,7 +7,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'github-repolist-clone',
+    title: 'realabdullah (Abdullahi Odesanmi) / Repositories . GitHub',
     htmlAttrs: {
       lang: 'en'
     },
