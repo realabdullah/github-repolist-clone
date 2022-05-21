019f11dd1748543065476afec1d67b9b8f78bd1d
realabdullah <abdulodesanmi@gmail.com>
2022-05-21 11:26:49 +0100

    Navbar

--- components/NavBar.vue
+++ components/NavBar.vue
@@ -31,9 +31,20 @@
     <div class="right__header">
       <form class="__search">
         <label for="search">
-          <input type="search" placeholder="Search">
-          <svg class="search__icon" xmlns="http://www.w3.org/2000/svg" width="22" height="20" aria-hidden="true">
-            <path fill="none" stroke="#979A9C" opacity=".4" d="M3.5.5h12c1.7 0 3 1.3 3 3v13c0 1.7-1.3 3-3 3h-12c-1.7 0-3-1.3-3-3v-13c0-1.7 1.3-3 3-3z"></path>
+          <input type="search" placeholder="Search" />
+          <svg
+            class="search__icon"
+            xmlns="http://www.w3.org/2000/svg"
+            width="22"
+            height="20"
+            aria-hidden="true"
+          >
+            <path
+              fill="none"
+              stroke="#979A9C"
+              opacity=".4"
+              d="M3.5.5h12c1.7 0 3 1.3 3 3v13c0 1.7-1.3 3-3 3h-12c-1.7 0-3-1.3-3-3v-13c0-1.7 1.3-3 3-3z"
+            ></path>
             <path fill="#979A9C" d="M11.8 6L8 15.1h-.9L10.8 6h1z"></path>
           </svg>
         </label>
@@ -55,6 +66,6 @@
 
 <script>
 export default {
-  name: 'NavBar'
-}
+  name: "NavBar",
+};
 </script>
