e8165469ee518dd3ca84fc18dbbf3e9dd3ca6479
realabdullah <abdulodesanmi@gmail.com>
2022-05-26 19:20:55 +0100

    dark mode(easy impl.)

--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="userTheme ? 'dark-mode' : 'light-mode'">
+  <div>
     <NavBar />
     <main>
       <DesktopAction />
@@ -18,51 +18,5 @@
 <script>
 export default {
   name: "IndexPage",
-
-  data() {
-    return {
-      mode: '',
-      userTheme: ''
-    }
-  },
-  
-  methods: {
-    getMediaPreference () {
-      const hasDarkPreference = window.matchMedia(
-        "(prefers-color-scheme: dark)"
-      ).matches
-      if (hasDarkPreference) {
-        this.mode = 'Dark Mode'
-        console.log(this.mode)
-        return "dark-theme"
-      } else {
-        this.mode = 'Light Mode'
-        console.log(this.mode)
-        return "light-theme"
-      }
-    },
-
-    setTheme(theme) {
-      localStorage.setItem("user-theme", theme)
-      this.userTheme = theme
-      document.documentElement.className = theme
-    },
-
-    toggleTheme () {
-      const activeTheme = localStorage.getItem("user-theme")
-      if (activeTheme === "light-mode") {
-        setTheme("dark-mode")
-        this.mode = "Dark Mode"
-      } else {
-        setTheme("light-mode")
-        this.mode = "Light Mode"
-      }
-    }
-  },
-
-  mounted() {
-    const initUserTheme = this.getMediaPreference()
-    this.setTheme(initUserTheme)
-  }
 };
 </script>
